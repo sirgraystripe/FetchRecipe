03760b579946d5715575ff3620bf80e196dc63dc
Peter Kos <sirgraystripe@icloud.com>
2024-09-01 15:29:06 -0700

    Initial Commit

---
+++ FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Peter Kos on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
